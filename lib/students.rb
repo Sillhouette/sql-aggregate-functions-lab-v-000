41365268f4f45a023d7fc5cb2565dba9d2f37588
Richard Melchior <raustin.melchior@gmail.com>
2018-06-12 20:06:57 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,13 +9,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "SELECT AVERAGE(gpa) FROM students"
+  "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
-  "SELECT ADD(tardies) FROM students"
+  "SELECT SUM(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
-  "SELECT AVERAGE(gpa) FROM students WHERE grade = 9"
+  "SELECT AVG(gpa) FROM students WHERE grade = 9"
 end
